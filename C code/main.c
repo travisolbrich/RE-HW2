9258b6adfa96f8c4ca31e43b789612deb227ce0b
Travis Olbrich <travis.olbrich@outlook.com>
2015-02-16 20:33:53 -0600

    Exercise variables

--- C code/main.c
+++ C code/main.c
@@ -100,11 +100,16 @@ int main()
 
 void data_types()
 {
-	// Basic types
-	char c = "a";
-	signed char sc = "a";
-	unsigned char uc = "a";
+	// Manipulate character type
+	char c = 'a';
+	signed char sc = 'a';
+	unsigned char uc = 'a';
 
+	c = 'b';
+	sc++;
+	uc++;
+
+	// Maniuplate int type
 	int i = 1000;
 	unsigned int ui = 1000;
 	short int si = 1000;
@@ -114,12 +119,31 @@ void data_types()
 	long long int lli = 1000;
 	unsigned long long int ulli = 1000;
 
+	i += 100;
+	ui += 100;
+	si += 100;
+	usi += 100;
+	li += 100;
+	uli += 100;
+	lli += 100;
+	lli += 100;
+	ulli += 100;	
+
+	// Manipulate foat type
 	float f = 1000.1001;
+	f += 1.1;
 	
+	// Manipulate double type
 	double d = 1000.1001;
 	long double ld = 1000.1001;
 	
+	d += 1.1;
+	ld += 1.1;
+
+	// Manipulate boolean type
 	bool b = true;
+	b = false;
+
 
 	// Structs
 	struct s
@@ -130,18 +154,19 @@ void data_types()
 		int month;
 		int year;
 	};
+	struct s s_example = { "Example Student", 123456789, 1, 1, 2001 };
 
-	// Arrays
-	char ca[30];
-	int ia[30];
-	float fa[30];
-	double da[30];
+	// Arrays (manipulated elsewhere)
+	char ca[] = { 'a', 'b', 'c' };
+	int ia[] = { 1, 2, 3 };
+	float fa[] = { 1.0, 2.0, 3.0 };
+	double da[] = { 1.0, 2.0, 3.0 };
 
-	// Pointers
-	char *cp = &c;
-	int *ip = &i;
-	float *fp = &f;
-	double *dp = &d;
+	// Pointers (manipulated elsewhere)
+	char * cp = &c;
+	int * ip = &i;
+	float * fp = &f;
+	double * dp = &d;
 }
 
 /*
